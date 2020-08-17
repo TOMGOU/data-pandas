a6ce074c7ff225b0cc738b1e90169235f77d0813
tommytang <tommy.tang@lixinchuxing.com>
2020-08-17 17:14:04 +0800

    fix: 添加线程

--- gui.py
+++ gui.py
@@ -1,7 +1,8 @@
 import win_unicode_console
 win_unicode_console.enable()
 import sys,os
-from PyQt5.QtCore import *
+# from PyQt5.QtCore import *
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
 from PyQt5.QtWidgets import (QWidget, QPushButton, QLineEdit, QLabel, QApplication, QFileDialog)
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -14,6 +15,7 @@ class upload(QWidget):
   def __init__(self):
     super(upload, self).__init__()
     self.initUI()
+
   def initUI(self):
      #关键词输入框和提示
     self.keyLabel = QLabel(self)
@@ -35,55 +37,86 @@ class upload(QWidget):
 
     #保存按钮，调取数爬取函数等
     self.save_btn = QPushButton('开始',self)
-    self.save_btn.move(120, 200)
+    self.save_btn.move(200, 200)
     self.save_btn.resize(140, 30)
     self.save_btn.clicked.connect(self.kick)
 
     #执行成功返回值显示位置设置
-    self.result_le = QLabel(self)
+    self.result_le = QLabel('', self)
     self.result_le.move(30, 270)
     self.result_le.resize(340, 30)
+    self.result_le.setStyleSheet('color: blue;')
 
     #整体界面设置
     self.setGeometry(400, 400, 400, 400)
-    self.setWindowTitle('小猪找货数据爬取')#设置界面标题名
+    self.setWindowTitle('小猪找货')#设置界面标题名
     self.show()
+
   #保存的excel文件名称，要写上后缀名
   def select_target(self):
     target,fileType = QFileDialog.getSaveFileName(self, "选择保存路径", "C:/")
     self.target_le.setText(str(target))
+
+  def set_label_func(self, text):
+    self.result_le.setText(text)
+
   def kick(self):
     key_words = self.key_le.text().strip()#关键词
     target = self.target_le.text().strip()#保存路径
-    self.fetchData(key_words, target)
-  def fetchData(self, key_words, target):
-    print('关键词:', key_words)
-    print('保存路径:', target)
+    self.set_label_func('请勿关闭浏览器，数据疯狂获取中...')
+    self.my_thread = MyThread(key_words, target, self.set_label_func)#实例化线程对象
+    self.my_thread.start()#启动线程
+    self.my_thread.my_signal.connect(self.set_label_func)
+
+class MyThread(QThread):#线程类
+  my_signal = pyqtSignal(str)  #自定义信号对象。参数str就代表这个信号可以传一个字符串
+  def __init__(self, key_words, target, set_label_func):
+    super(MyThread, self).__init__()
+    self.key_words = key_words
+    self.target = target
+    self.set_label_func = set_label_func
+
+  def run(self): #线程执行函数
+    length = self.fetchData(self.key_words, self.target, self.set_label_func)
+    if length != 0:
+      string = '获取了' + str(length) + '条数据，请直接查看excel表格！'
+    else: 
+      string = '没有抓取到数据，请尝试其他关键词！'
+    self.my_signal.emit(string)  #释放自定义的信号
+
+  def fetchData(self, key_words, target, set_label_func):
     browser = webdriver.Chrome()
     browser.get('https://z.vanmmall.com/?from=singlemessage&isappinstalled=0')
     browser.find_element_by_id('kw').send_keys(key_words)
     search = browser.find_element_by_id('btn_search')
     search.click()
-    time.sleep(2)
+    self.sleep(2)
     while not self.isElementExist(browser, '.dropload-noData'):
-      browser.execute_script("window.scrollTo(0,document.body.scrollHeight);")
-      time.sleep(1)
-    time.sleep(2)
+      browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+      self.sleep(1)
     date = browser.find_elements_by_css_selector('td.date')
     shop = browser.find_elements_by_css_selector('td.shop div.name')
     msg = browser.find_elements_by_css_selector('td.msg')
     wechat = browser.find_elements_by_css_selector('td.call div button.wx')
     mobile = browser.find_elements_by_css_selector('td.call div button.phone')
     data = []
-    for index in range(len(date)):
+    cal_str = '已经获取了' + str(len(data)) + '条数据，正在合成excel，请耐心等待！'
+    self.set_label_func(cal_str)
+    data_length = len(date)
+    for index in range(data_length):
       date_text = date[index].get_attribute('textContent')
       shop_text = shop[index].get_attribute('textContent')
       msg_text = msg[index].get_attribute('textContent')
       wechat_text = wechat[index].get_attribute("data-msg")
       mobile_text = mobile[index].get_attribute("data-msg")
       data.append([date_text, shop_text, msg_text, wechat_text, mobile_text])
-    print(data)
+      process_str = '当前数据合成进度： ' + str(index + 1) + ' / ' + str(data_length)
+      self.set_label_func(process_str)
+    browser.quit()
+    self.set_label_func('excel表格保存中...')
     self.saveDataToExcel(data, target)
+    return data_length
+
   def isElementExist(self, browser, element):
     flag=True
     try:
@@ -92,7 +125,10 @@ class upload(QWidget):
     except:
       flag=False
       return flag
+
   def saveDataToExcel(self, data, target):
+    if len(data) == 0:
+      return
     data_df = pd.DataFrame(data)
     data_df.columns = ['日期','商家','产品信息+报价','微信号','手机号']
     writer = pd.ExcelWriter(target)
@@ -102,4 +138,5 @@ class upload(QWidget):
 if __name__=="__main__":
   app = QApplication(sys.argv)
   ex = upload()
+  ex.show()
   sys.exit(app.exec_())
